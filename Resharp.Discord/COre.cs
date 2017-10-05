11a8b54fe635501f8170cf9f4189013d8781886a
Dionne Pasion <allure.success@gmail.com>
2017-10-05 15:05:42 -0600

    Added start of core engine

---
+++ Resharp.Discord/COre.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using Newtonsoft.Json;
+
+namespace Resharp.Discord
+{
+    public class Core
+    {
+        public DiscordClient Client { get; set; }
+
+        public static void Main(string[] args)
+            => new Core();
+
+
+       /*
+        * Run a console that runs the bot asyncronously
+        * Reads configuration file to 
+        */
+        public async Task RunBotAsync()
+        {
+            //read the configuration settings
+            var json = "";
+            using(var fs = File.OpenRead("config.json"))
+            {
+                using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                {
+                    json = await sr.ReadToEndAsync();
+                }
+            }
+
+            var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+        }
+
+        public struct ConfigJson
+        {
+            [JsonProperty("Token")]
+            public string Token { get; private set; }
+            
+            [JsonProperty("prefix")]
+            public string CommandPrefix { get; private set; }
+        }
+    }
+}
